e7ae4192ff4d5345794398933faddbb77a6be330
Praveen Raju <praveenraju2889@gmail.com>
2024-01-29 13:53:16 +0400

    Initial Commit

---
+++ SpeedCal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeedCal
+//
+//  Created by Praveen Raju on 29/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
